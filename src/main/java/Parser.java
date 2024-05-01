860da9b19e9dc0b2d7c996572e2f177875b20207
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-01 15:06:47 -0700

    Added all tokens to str_to_tokens HashMap

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -225,9 +225,37 @@ class Parser {
                 List<Token> list = new ArrayList<>();
                 Map<String, TokenType> str_to_tokens = new HashMap<>();
 
-
                 str_to_tokens.put("End_of_input", TokenType.End_of_input);
-                // finish creating your Hashmap. I left one as a model
+                str_to_tokens.put("Op_multiply", TokenType.Op_multiply);
+                str_to_tokens.put("Op_divide", TokenType.Op_divide);
+                str_to_tokens.put("Op_mod", TokenType.Op_mod);
+                str_to_tokens.put("Op_add", TokenType.Op_add);
+                str_to_tokens.put("Op_subtract", TokenType.Op_subtract);
+                str_to_tokens.put("Op_negate", TokenType.Op_negate);
+                str_to_tokens.put("Op_not", TokenType.Op_not);
+                str_to_tokens.put("Op_less", TokenType.Op_less);
+                str_to_tokens.put("Op_lessequal", TokenType.Op_lessequal);
+                str_to_tokens.put("Op_greater", TokenType.Op_greater);
+                str_to_tokens.put("Op_greaterequal", TokenType.Op_greaterequal);
+                str_to_tokens.put("Op_equal", TokenType.Op_equal);
+                str_to_tokens.put("Op_notequalt", TokenType.Op_notequal);
+                str_to_tokens.put("Op_assign", TokenType.Op_assign);
+                str_to_tokens.put("Op_and", TokenType.Op_and);
+                str_to_tokens.put("Op_or", TokenType.Op_or);
+                str_to_tokens.put("Keyword_if", TokenType.Keyword_if);
+                str_to_tokens.put("Keyword_else", TokenType.Keyword_else);
+                str_to_tokens.put("Keyword_while", TokenType.Keyword_while);
+                str_to_tokens.put("Keyword_print", TokenType.Keyword_print);
+                str_to_tokens.put("Keyword_putc", TokenType.Keyword_putc);
+                str_to_tokens.put("LeftParen", TokenType.LeftParen);
+                str_to_tokens.put("RightParen", TokenType.RightParen);
+                str_to_tokens.put("LeftBrace", TokenType.LeftBrace);
+                str_to_tokens.put("RightBrace", TokenType.RightBrace);
+                str_to_tokens.put("Semicolon", TokenType.Semicolon);
+                str_to_tokens.put("Comma", TokenType.Comma);
+                str_to_tokens.put("Identifier", TokenType.Identifier);
+                str_to_tokens.put("Integer", TokenType.Integer);
+                str_to_tokens.put("String", TokenType.String);
 
                 Scanner s = new Scanner(new File("src/main/resources/hello.lex"));
                 String source = " ";
